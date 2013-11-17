fb1f45ae3a67d255e7f45031551ebaadaca73c08
Daiwei Lu <daiweilu123@gmail.com>
2013-11-17 16:54:23 -0500

    Translate birthdate to zodiac names

--- public/get_friends_data.pl
+++ public/get_friends_data.pl
@@ -3,6 +3,8 @@
 use CGI qw(:standard);
 use LWP::UserAgent; # install Mozilla::CA module to enable SSL calls
 use JSON;
+use Date::Horoscope;
+use Date::Manip;
 use Data::Dumper;
 use strict;
 use warnings;
@@ -18,7 +20,13 @@ my $response = $ua->get("https://graph.facebook.com/me?fields=friends.fields(bir
 
 print header(-type => "application/json", -charset => "utf-8");
 if ($response->is_success) {
-  print $response->decoded_content;
+  my %data = %{ decode_json($response->decoded_content) };
+  foreach (@{ $data{'friends'}{'data'} }) {
+    my $birthday = $_->{'birthday'};
+    $birthday =~ s/\//\-/;
+    $_->{'zodiac_name'} = Date::Horoscope::locate($birthday);
+  }
+  print encode_json(\%data);
 }
 else {
   print $response->decoded_content;
