83ce5253fc0a3af2a0f5df7ff7065106fe6f323c
Daiwei Lu <daiweilu123@gmail.com>
2013-11-17 18:15:13 -0500

    Transform app to angular app

--- public/get_friends_data.pl
+++ public/get_friends_data.pl
@@ -16,7 +16,7 @@ my $ua = LWP::UserAgent->new;
 $ua->timeout(10);
 $ua->env_proxy;
 
-my $response = $ua->get("https://graph.facebook.com/me?fields=friends.fields(birthday,name)&access_token=$access_token");
+my $response = $ua->get("https://graph.facebook.com/me?fields=friends.fields(birthday,gender)&access_token=$access_token");
 
 print header(-type => "application/json", -charset => "utf-8");
 if ($response->is_success) {
